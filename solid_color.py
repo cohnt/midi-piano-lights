eceb671062d08192e3107b424ac6eaf7d64bb1f9
Thomas Cohn <cohnt@umich.edu>
2020-11-20 21:27:20 -0500

    More general script to do a single, solid color.

---
+++ solid_color.py
@@ -0,0 +1,33 @@
+import pygame.midi
+import board
+import neopixel
+
+n_pixels = 177
+n_keys = 88.0
+min_key = 21
+max_key = 108
+color = (255, 0, 0)
+
+key_status = [False for _ in range(n_keys)]
+keypress_times = [0 for _ in range(n_keys)]
+brightness = [0 for _ in range(n_keys)]
+
+pixels = neopixel.NeoPixel(board.D18, n_pixels, brightness=1.0, auto_write=False, pixel_order=neopixel.GRB)
+
+def key_idx_to_strip_leds(idx):
+	# Map to [0, 1)
+	low = float(idx) / n_keys
+	high = float(idx+1) / n_keys
+	# Map to [0, n_pixels)
+	low = low * n_pixels
+	high = high * n_pixels
+	return range(low, high)
+
+def note_to_key_idx(note_number):
+	# Midi input is in range [min_key, max_key]
+	return note_number - min_key
+
+def number_to_note(number):
+	# Only needed for debugging
+	notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
+	return notes[number%12]
