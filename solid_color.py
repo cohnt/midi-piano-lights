ed280830d72caa111df8107be527a693c75a6dbd
Thomas Cohn <cohnt@umich.edu>
2020-11-20 22:02:22 -0500

    Pixel drawing loop.

--- solid_color.py
+++ solid_color.py
@@ -2,6 +2,7 @@ import pygame.midi
 import board
 import neopixel
 import math
+import time
 
 n_pixels = 177
 n_keys = 88
@@ -60,17 +61,48 @@ try:
 
 			status = data[0]
 			note_number = data[1]
-			velocity = data[2]
+			velocity = data[2] # For now, velocity is just used to determine if it was a press or release
 
 			if status == important_statuses["time_update"]:
 				# We can ignore time updates
 				continue
 			elif status == important_statuses["pedal"]:
+				# Not using the pedal for now, but print it for debugging purposes
 				down = velocity > 0
 				print("T=%d\tPedal %s" % (timestamp, "Down" if down else "Up"))
 			elif status == important_statuses["note"]:
 				down = velocity > 0
-				print("T=%d\tNote=%d %s\tVel=%d" % (timestamp, note_number, "Down" if down else "Up", velocity))
+				print("T=%d\tNote=%d (%s) %s\tVel=%d" % (timestamp, note_number, number_to_note(note_number), "Down" if down else "Up", velocity))
+				# Update note status
+				key_idx = note_to_key_idx(note_number)
+				if down:
+					key_status[key_idx] = True
+					keypress_times[key_idx] = time.time()
+				else:
+					key_status[key_idx] = False
+					keypress_times[key_idx] = -1
+		#
+		# Update brightness levels
+		current_time = time.time()
+		for i in range(n_keys):
+			if(key_status[i] == False):
+				brightness[i] = 0
+			else:
+				brightness[i] = time_brightness_curve(current_time - keypress_times[i])
+		#
+		# Update the pixels
+		for i in range(n_keys):
+			this_key_brightness = brightness[i]
+			this_key_color = (
+				int(this_key_brightness * color[0]),
+				int(this_key_brightness * color[1]),
+				int(this_key_brightness * color[2]),
+			)
+			this_key_pixels = key_idx_to_strip_leds(i)
+			for j in range(len(this_key_pixels)):
+				pixels[j] = this_key_color
+		pixels.show()
+	#
 except KeyboardInterrupt:
 	print("Exiting...")
 	input_device.close()
