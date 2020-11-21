f2c4a96a54839044320ab42c86dcf9421414aea8
Thomas Cohn <cohnt@umich.edu>
2020-11-20 21:29:55 -0500

    Initial event loop.

--- solid_color.py
+++ solid_color.py
@@ -31,3 +31,19 @@ def number_to_note(number):
 	# Only needed for debugging
 	notes = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
 	return notes[number%12]
+
+pygame.midi.init()
+my_input = pygame.midi.Input(3)
+
+try:
+	while True:
+		# Get new MIDI input if any is available
+		if input_device.poll():
+			event = input_device.read(1)[0]
+			data = event[0]
+			timestamp = event[1]
+			note_number = data[1]
+			velocity = data[2]
+			print("%d\t%d\t%d" % (timestamp, note_number, velocity))
+except KeyboardInterrupt:
+	print("Exiting...")
