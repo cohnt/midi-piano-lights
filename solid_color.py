08616584fac4680e3934e80619183cbd35d27c1b
Thomas Cohn <cohnt@umich.edu>
2020-11-20 22:51:05 -0500

    Still messing with the brightness curve.

--- solid_color.py
+++ solid_color.py
@@ -53,7 +53,7 @@ def time_brightness_curve(t):
 		out = 1 - (1 / denom)
 		return out * 2
 	else:
-		denom = 1 + math.exp(-2 - (0.5 * (t - 0.25)))
+		denom = 1 + math.exp(-2 - (2 * (t - 0.25)))
 		out = 1 - (1 / denom)
 		return out * 2
 
